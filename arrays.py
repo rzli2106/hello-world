76ee1a5df5e34dd77d56f80e265292e0e099a324
Richard Li <rzli2106@gmail.com>
2026-09-14 20:14:55 -0400

    adding more info

--- arrays.py
+++ arrays.py
@@ -1,3 +1,49 @@
+#lists/arrays
+list = [1, 2, 3, 4, 5]
+list.append(6)
+list.append(7)
+list.pop()
+print(list)
+
+list.insert(1,7) #inserting 7 at index 1
+list[0] = 10 #changing value at index 0 to 10
+print(list)
+print(len(list)) #length of list
+print(list[0:3]) #slicing list from index 0 to 2'''
+
+
+
+#array for loops
+nums = [1, 2, 3, 4, 5]
+for i in range(len(nums)):
+    print(nums[i])
+
+for n in nums:
+    print(n)
+
+#index and value
+for i,n in enumerate(nums):
+    print(i, n)
+
+
+#multiple arrays
+nums1 = [1, 2, 3, 4, 5]
+nums2 = [6, 7, 8, 9, 10]
+for n1, n2 in zip(nums1, nums2):
+    print(n1, n2)
+
+#reversing an array
+nums2.reverse()
+print(nums2)
+
+#sorting an array
+nums3 = [2, 50, 4, 9, 1, 3]
+nums3.sort()
+print(nums3)
+nums3.sort(reverse=True)
+print(nums3)
+
+
 arr = ['jane', 'alice', 'charlie', 'dave', 'bob']
 arr.sort() # sorts by alphabetical order
 print(arr)
