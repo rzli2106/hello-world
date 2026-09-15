0d5b87d5db4bbf4058b780d85497ceedbd486bec
Richard Li <rzli2106@gmail.com>
2026-09-14 20:20:00 -0400

    Initial commit

---
+++ hash_sets.py
@@ -0,0 +1,19 @@
+# HashSets
+mySet = set()
+
+mySet.add(1)
+mySet.add(1) # hashsets cant have duplicates
+mySet.add(2)
+print(mySet)
+print(len(mySet)) # length of hashset
+
+print(1 in mySet) # used to search hashset
+
+mySet.remove(2)
+print(mySet)
+
+print(set[1,2,3,4]) # list to set
+
+mySet1 = {i for i in range(5)}
+print(mySet1)
+
